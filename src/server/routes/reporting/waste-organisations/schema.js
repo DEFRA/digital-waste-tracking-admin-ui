67c7f951af063458970cd417cde499b61aaf7393
David Shepherd <6839214+shepda@users.noreply.github.com>
2026-07-07 10:27:43 +0100

    DWTA-248 Download Waste Organisations CSV (#3)
    * DWT-248 Add @date-fns/utc and csv-stringify dependencies
    
    * DWTA-248 Add generateCsvWasteOrganisations() function to generate CSV data from an array of Waste Organisation objects
    
    * DWT-248 Update Waste Organisation controller to add CSV download
    The download is done in the same controller action so we get the benefit of fetching fresh data and a validation fallback (although as unlikely scenario) for the download and without having to duplicate the code
    
    * DWTA-248 Add Waste Organisation controller unit test to cover error messages
    
    * DWTA-248 Add BOM to CSV download
    This more reliably determines that the file is UTF-8 encoded when opened

--- src/server/routes/reporting/waste-organisations/schema.js
+++ src/server/routes/reporting/waste-organisations/schema.js
@@ -1,6 +1,7 @@
 import { isAfter } from 'date-fns'
 import Joi from 'joi'
 import { createIsoDate } from '../../../common/helpers/create-iso-date.js'
+import { downloadFormats } from '../../../common/constants/download-formats.js'
 
 export const wasteOrgansiationsReportingSchema = Joi.object({
   'date-from-Day': Joi.string().required().label('From date day'),
@@ -8,36 +9,43 @@ export const wasteOrgansiationsReportingSchema = Joi.object({
   'date-from-Year': Joi.string().required().label('From date year'),
   'date-to-Day': Joi.string().required().label('To date day'),
   'date-to-Month': Joi.string().required().label('To date month'),
-  'date-to-Year': Joi.string().required().label('To date year')
-}).custom((value, helpers) => {
-  const dateFrom = createIsoDate(
-    value['date-from-Day'],
-    value['date-from-Month'],
-    value['date-from-Year']
-  )
-  const dateTo = createIsoDate(
-    value['date-to-Day'],
-    value['date-to-Month'],
-    value['date-to-Year']
-  )
-
-  if (!(dateFrom instanceof Date)) {
-    return helpers.message('From date must be a valid date', {
-      local: { fieldName: 'date-from' }
-    })
-  }
-
-  if (!(dateTo instanceof Date)) {
-    return helpers.message('To date must be a valid date', {
-      local: { fieldName: 'date-to' }
-    })
-  }
-
-  if (isAfter(dateFrom, dateTo)) {
-    return helpers.message('To date must be later than From date', {
-      local: { fieldName: 'date-to' }
-    })
-  }
-
-  return value
+  'date-to-Year': Joi.string().required().label('To date year'),
+  download: Joi.string()
+    .valid(...Object.keys(downloadFormats))
+    .label('Download')
 })
+  .custom((value, helpers) => {
+    const dateFrom = createIsoDate(
+      value['date-from-Day'],
+      value['date-from-Month'],
+      value['date-from-Year']
+    )
+    const dateTo = createIsoDate(
+      value['date-to-Day'],
+      value['date-to-Month'],
+      value['date-to-Year']
+    )
+
+    if (!(dateFrom instanceof Date)) {
+      return helpers.message('From date must be a valid date', {
+        local: { fieldName: 'date-from' }
+      })
+    }
+
+    if (!(dateTo instanceof Date)) {
+      return helpers.message('To date must be a valid date', {
+        local: { fieldName: 'date-to' }
+      })
+    }
+
+    if (isAfter(dateFrom, dateTo)) {
+      return helpers.message('To date must be later than From date', {
+        local: { fieldName: 'date-to' }
+      })
+    }
+
+    return value
+  })
+  .messages({
+    'any.only': `{{ #label }} must be one of: ${Object.keys(downloadFormats).join(', ')}`
+  })
